e8ffd7fae5cc13c63a75d4aeab11f77d56247692
Eddy Matlock-Mahon <eddy.mahon82@gmail.com>
2018-10-29 20:46:35 -0700

    Added commands to the script

--- myscript.sh
+++ myscript.sh
@@ -2,3 +2,12 @@
 
 echo "hello ITC-136"
 echo "edited in GitHub"
+
+echo "this is your current working directory:"
+pwd
+
+echo "this is the current version of linux you are running:"
+uname -a
+
+echo "These are your current logged in users:"
+who
