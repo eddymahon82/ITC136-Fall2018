03c6ef22d750557bda3640e2ac7797183b8b3fa7
Eddy Matlock-Mahon <36487159+eddymahon82@users.noreply.github.com>
2018-11-19 20:11:21 -0800

    corrected spelling on line 7

--- stats.sh
+++ stats.sh
@@ -4,7 +4,7 @@ diskused=$( df -h | grep "/dev/sda1" | awk '{print $5}' )
 
 memfree=$( free -mh | grep Mem: | awk '{print $7}' )
 
-connections=$( nenstat | grep tcp )
+connections=$( netstat | grep tcp )
 
 users=$( who )
 
