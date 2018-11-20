594461f5a6893d48dd4c2b31cb30525432abd37a
Eddy Matlock-Mahon <36487159+eddymahon82@users.noreply.github.com>
2018-11-19 19:36:52 -0800

    Scripting Lab 0 - Class Assignment

---
+++ ScriptLab0.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+df -h
+* how much disk space has been used
+
+free -h
+* how much memory is free
+
+netstat
+* how many connections there are to the current machine
+
+who
+* who is logged in
